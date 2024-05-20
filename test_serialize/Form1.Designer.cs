5f861d9d7a395a42487e9d17a4f7a916556c44ef
JirawatChayayan <51415869+JirawatChayayan@users.noreply.github.com>
2024-05-20 15:09:47 +0700

    Add project files.

---
+++ test_serialize/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace test_serialize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hSmartWindowControl1 = new HalconDotNet.HSmartWindowControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hSmartWindowControl1
+            // 
+            this.hSmartWindowControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.hSmartWindowControl1.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.hSmartWindowControl1.HDoubleClickToFitContent = true;
+            this.hSmartWindowControl1.HDrawingObjectsModifier = HalconDotNet.HSmartWindowControl.DrawingObjectsModifier.None;
+            this.hSmartWindowControl1.HImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hSmartWindowControl1.HKeepAspectRatio = true;
+            this.hSmartWindowControl1.HMoveContent = true;
+            this.hSmartWindowControl1.HZoomContent = HalconDotNet.HSmartWindowControl.ZoomContent.WheelForwardZoomsIn;
+            this.hSmartWindowControl1.Location = new System.Drawing.Point(9, 9);
+            this.hSmartWindowControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.hSmartWindowControl1.Name = "hSmartWindowControl1";
+            this.hSmartWindowControl1.Size = new System.Drawing.Size(667, 562);
+            this.hSmartWindowControl1.TabIndex = 0;
+            this.hSmartWindowControl1.WindowSize = new System.Drawing.Size(667, 562);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(682, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ResetImg";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(682, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "GenRegion";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(682, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 51);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Serialize";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(682, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 51);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "De-Serialize";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 580);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hSmartWindowControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HalconDotNet.HSmartWindowControl hSmartWindowControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ test_serialize/Form1.cs
@@ -0,0 +1,107 @@
+﻿using HalconDotNet;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace test_serialize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        HImage img = null;
+        HRegion region = null;
+        string b64StreamData = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //read
+            if (img == null) 
+            {
+                img = new HImage();
+            }
+            img.Dispose();
+            img.ReadImage("printer_chip/printer_chip_01");
+            img.GetImageSize(out HTuple width, out HTuple height);
+            HOperatorSet.SetSystem("width",width);
+            HOperatorSet.SetSystem ("height",height);
+            hSmartWindowControl1.HalconWindow.ClearWindow();
+            hSmartWindowControl1.HalconWindow.DispObj(img);
+            hSmartWindowControl1.SetFullImagePart();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(img == null)
+            {
+                return;
+            }
+            if(region == null)
+            {
+                region = new HRegion();
+            }
+            region.Dispose();
+            region = img.Threshold(128.0, 255.0);
+
+            hSmartWindowControl1.HalconWindow.ClearWindow();
+            hSmartWindowControl1.HalconWindow.DispObj(img);
+            hSmartWindowControl1.HalconWindow.SetColor("red");
+            hSmartWindowControl1.HalconWindow.DispObj(region);
+            hSmartWindowControl1.SetFullImagePart();
+            b64StreamData = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (img == null)
+            {
+                return;
+            }
+            if (region == null)
+            {
+                return;
+            }
+            MemoryStream ms = new MemoryStream();
+            HObject obj = new HObject(region);
+            obj.Serialize(ms);
+            ms.Flush();
+            ms.Position = 0;
+            byte[] bytearr = ms.ToArray();
+            b64StreamData = Convert.ToBase64String(bytearr);
+            ms.Close();
+            obj.Dispose();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (img == null)
+            {
+                return;
+            }
+            if (b64StreamData == null)
+            {
+                return;
+            }
+            hSmartWindowControl1.HalconWindow.ClearWindow();
+            hSmartWindowControl1.HalconWindow.DispObj(img);
+            hSmartWindowControl1.HalconWindow.SetColor("yellow");
+            byte[] byteArray = Convert.FromBase64String(b64StreamData);
+            MemoryStream memoryStream = new MemoryStream(byteArray);
+            HObject reg = HObject.Deserialize(memoryStream);
+            memoryStream.Close();
+            hSmartWindowControl1.HalconWindow.DispObj(reg);
+            hSmartWindowControl1.SetFullImagePart();
+            reg.Dispose();
+        }
+    }
+}
